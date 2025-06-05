18bf729b2382b054ce80cc4cc30f48a4ad07f4d3
Rodrigo Ferraro <neuroticks@gmail.com>
2025-06-05 10:09:10 -0300

    cadastro de usuário completo

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,46 @@
+import { AppError } from "@/utils/AppError"
+import { Request, Response } from "express"
+import { prisma } from "@/database/prisma"
+import { hash } from "bcrypt"
+import { z } from "zod"
+
+class UsersController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists")
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    const { password: _, ...userWithoutPassword } = user
+
+    return response.status(201).json(userWithoutPassword)
+  }
+  async index(request: Request, response: Response) {
+    const l_users = await prisma.user.findMany()
+
+    return response.json({ users: l_users })
+  }
+
+
+}
+
+export { UsersController }
